327d090bffdddaf919dd8dd890f11704dcc529a3
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-05-31 16:36:36 -0300

    D11.js, EF.js upDate

--- src/Slide/componentsSlide/D11/ElementosFilhosD11/EF.js
+++ src/Slide/componentsSlide/D11/ElementosFilhosD11/EF.js
@@ -11,13 +11,23 @@ var EF = {
             </div>             
     },
     D111 : {
-        img111 : <img  className="img111 imgd" id ='img111'  /*onMouseOver={Funções.events.over.img111.f1} onMouseOut={Funções.events.out.img111.f1}*/ alt=''/>
+        img111 :             
+        <div  className="img111 imgd" id ='img111'  /*onMouseOver={Funções.events.over.img111.f1} onMouseOut={Funções.events.out.img111.f1}*/ alt=''>
+            <img  className="img111 imgd imgdis" id ='img111'  /*onMouseOver={Funções.events.over.img111.f1} onMouseOut={Funções.events.out.img111.f1}*/ src="./images/fashion-model-in-fur.jpg" alt=''/>  
+        </div>
     },
     D112 : {
-        img112 : <img  className="img112 imgd" id ='img112'  /*onMouseOver={Funções.events.over.img112.f1} onMouseOut={Funções.events.out.img112.f1}*/ alt=''/>
+        img112 : 
+        <div  className="img112 imgd" id ='img112'  /*onMouseOver={Funções.events.over.img112.f1} onMouseOut={Funções.events.out.img112.f1}*/ alt=''>
+            <img  className="img112 imgd imgdis" id ='img112'  /*onMouseOver={Funções.events.over.img112.f1} onMouseOut={Funções.events.out.img112.f1}*/ src="./images/man-in-white-and-light-tan-outfit.jpg" alt=''/>  
+        </div>
     },
     D113 : {
-        img113 : <img  className="img113 imgd" id ='img113'  /*onMouseOver={Funções.events.over.img113.f1} onMouseOut={Funções.events.out.img113.f1}*/ alt=''/>
+        img113 : 
+        <div  className="img113 imgd" id ='img113'  /*onMouseOver={Funções.events.over.img113.f1} onMouseOut={Funções.events.out.img113.f1}*/ alt=''>
+            <img  className="img113 imgd imgdis" id ='img113'  /*onMouseOver={Funções.events.over.img113.f1} onMouseOut={Funções.events.out.img113.f1}*/ src="./images/young-woman-in-red.jpg" alt=''/>        
+        </div>
+    
     },
 };
 
