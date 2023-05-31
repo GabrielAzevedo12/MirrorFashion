f151c1bd5481c8701c1d41cfd768de5139722a17
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-05-31 15:30:51 -0300

    db.json update

--- src/Slide/Funções/Mov/src.js
+++ src/Slide/Funções/Mov/src.js
@@ -5,7 +5,62 @@ var scrd = (ElementoHtml,scrdm,irmao) => {
     Pai.scrollTo({ left: m*iw, top: 0, behavior: 'smooth'}) 
 };
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-export  { scrd }
+//DEF
+/*
+
+*Tipos:
+
+ElementoScroll : Objeto (Elemento html) 
+TypeScroll : String (Determinara o tipo de scroll no elemento)
+Incre : Number (Valor que será incrementado ao valor da posição 
+atual da tela na horizontal ou vertical)
+
+*/
+
+var srcrdg = (Incre,ElementoScroll,TypeScroll) => {
+
+    let ValorIncre = ElementoScroll[TypeScroll];
+
+    if(ValorIncre >= 0 && ValorIncre !== 2*window.innerWidth) {
+
+        let ScrollTotal = ValorIncre + Incre;
+
+        if(TypeScroll === "scrollLeft") {
+            ElementoScroll.scrollTo({ left: ScrollTotal, top: 0, behavior: 'smooth'})
+        } else if (TypeScroll === "scrollTop") {
+            ElementoScroll.scrollTo({ left: 0, top: ScrollTotal, behavior: 'smooth'})
+        }
+
+    }
+
+    if(ValorIncre === 2*window.innerWidth) {
+
+        let ScrollTotal = ValorIncre - Incre;
+
+        if(TypeScroll === "scrollLeft") {
+            ElementoScroll.scrollTo({ left: 0, top: 0, behavior: 'smooth'})
+        } else if (TypeScroll === "scrollTop") {
+            ElementoScroll.scrollTo({ left: 0, top: ScrollTotal, behavior: 'smooth'})
+        }   
+
+    }
+    
+
+}
+
+/*
+var $ = (NomeDaIndentificão) => { 
+          var ElementoHtml = document.querySelector(NomeDaIndentificão); // BOM.
+         return ElementoHtml; 
+}
+
+var teste_srcrdg = () => {
+    const w = window.innerWidth;
+    srcrdg(w,$(".Div11"),"scrollLeft")    
+}
+setInterval(teste_srcrdg,3000)*/
+
+export  { scrd, srcrdg }
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Teste
 //ML2v3($('.Div5'))
