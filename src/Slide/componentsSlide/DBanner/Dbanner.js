c452f5c426664a92f0e1cbfa74d7b4e1c9d1087a
gazevedo996 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-06-16 23:19:24 +0000

    16/06/2023

--- src/Slide/componentsSlide/DBanner/Dbanner.js
+++ src/Slide/componentsSlide/DBanner/Dbanner.js
@@ -1,18 +1,26 @@
 import "./css/Dbanner.css";
 //Fase : Teste
 const Dbanner = () => {
+  const display_none = { display: "none" };
   return (
     <>
       <div className="DbannerPrincipal">
-        <div className="DbannerTíd1 DbannerTíd">
+        <div className="DbannerTíd1 DbannerTíd" style={display_none}>
           <p className="bannerTíd1 bannerTíd1A">
             {" "}
             Sejá Bem Vindo a Mirror Fashion
           </p>
         </div>
-        <span className="bannerSpan bannerSpanA" ></span>
-        <div className="DbannerTíd2 DbannerTíd">
+        <span className="bannerSpan bannerSpanA"></span>
+        <div className="DbannerTíd2 DbannerTíd" style={display_none}>
           <p className="bannerTíd2"></p>
+        </div>
+        <div className="gridTD">
+          <div className="gridTF1 gridTF"></div>
+          <div className="gridTF2 gridTF"></div>
+          <div className="gridTF3 gridTF"></div>
+          <div className="gridTF4 gridTF"></div>
+          <div className="gridTF5 gridTF"></div>
         </div>
       </div>
     </>
