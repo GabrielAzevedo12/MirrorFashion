59feba537044db47058210d89743319659b98571
GabrielAzevedo12 Vieira <gazevedo996@gmail.com>
2023-06-30 11:58:38 +0000

    Fri Jun 30 11:57:53 AM UTC 2023

---
+++ src/Slide/componentsSlide/gridTD/1/gtfB/f/gtfhFB.js
@@ -0,0 +1,12 @@
+/*#Comentários*/
+  //Funções gtfB
+  /*#Obj_Main*/  
+  const gtfB_funções = {
+    /*#f1*/
+    teste : () => {
+        console.log('Esse é o objeto que contém métodos do hook gtfB')
+    }
+  }
+  /*#Export*/
+  export { gtfB_funções }
+  
---
+++ src/Slide/componentsSlide/gridTD/1/gtfB/gtfhB.js
@@ -0,0 +1,58 @@
+/*#import*/
+import './css/gtfhB.css'
+import './css/gtfhBA.css'
+import {
+  Adicionar_class,
+  Aumentar_ElementoPai,
+  Diminuir_ElementoPai,
+  gridTN,
+  Analise_class
+} from '../../../DBanner/f/f.js'
+/*#Hook*/
+const GridTFB = () => {
+return (
+    <div className='gridTFB gridTF animation1_gridTF animation1_gridTFB'
+    /*onClick={gridTN.gridTNB.Disparar_Evento_Quando.Click}*/
+    /*onMouseOut={gridTN.gridTNB.Disparar_Evento_Quando.Out}*/
+    /*onMouseOver={gridTN.gridTNB.Disparar_Evento_Quando.Over}*/
+    /*onDrag={gridTN.gridTNB.Disparar_Evento_Quando.Drag}*/
+    /*onDrop={gridTN.gridTNB.Disparar_Evento_Quando.Drop}*/
+    /*onDragStart={gridTN.gridTNB.Disparar_Evento_Quando.DragStart}*/
+    /*onDragOver={gridTN.gridTNB.Disparar_Evento_Quando.DragOver*/>
+            <div 
+            className='gridTNB1 gridTNB animation1_gridTNB animation1_gridTNB1'
+            /*onClick={gridTN.gridTNB.Disparar_Evento_Quando.Click}*/
+            /*onMouseOut={gridTN.gridTNB.Disparar_Evento_Quando.Out}*/
+            /*onMouseOver={gridTN.gridTNB.Disparar_Evento_Quando.Over}*/
+            /*onDrag={gridTN.gridTNB.Disparar_Evento_Quando.Drag}*/
+            /*onDrop={gridTN.gridTNB.Disparar_Evento_Quando.Drop}*/
+            /*onDragStart={gridTN.gridTNB.Disparar_Evento_Quando.DragStart}*/
+            /*onDragOver={gridTN.gridTNB.Disparar_Evento_Quando.DragOver*/>
+              subdestaque-11
+            </div>
+            <div className='gridTNB2 gridTNB animation1_gridTNB animation1_gridTNB2'
+            /*onClick={gridTN.gridTNB.Disparar_Evento_Quando.Click}*/
+            /*onMouseOut={gridTN.gridTNB.Disparar_Evento_Quando.Out}*/
+            /*onMouseOver={gridTN.gridTNB.Disparar_Evento_Quando.Over}*/
+            /*onDrag={gridTN.gridTNB.Disparar_Evento_Quando.Drag}*/
+            /*onDrop={gridTN.gridTNB.Disparar_Evento_Quando.Drop}*/
+            /*onDragStart={gridTN.gridTNB.Disparar_Evento_Quando.DragStart}*/
+            /*onDragOver={gridTN.gridTNB.Disparar_Evento_Quando.DragOver*/>
+              subdestaque-12
+            </div>
+            <div className='gridTNB3 gridTNB animation1_gridTNB animation1_gridTNB3'
+            /*onClick={gridTN.gridTNB.Disparar_Evento_Quando.Click}*/
+            /*onMouseOut={gridTN.gridTNB.Disparar_Evento_Quando.Out}*/
+            /*onMouseOver={gridTN.gridTNB.Disparar_Evento_Quando.Over}*/
+            /*onDrag={gridTN.gridTNB.Disparar_Evento_Quando.Drag}*/
+            /*onDrop={gridTN.gridTNB.Disparar_Evento_Quando.Drop}*/
+            /*onDragStart={gridTN.gridTNB.Disparar_Evento_Quando.DragStart}*/
+            /*onDragOver={gridTN.gridTNB.Disparar_Evento_Quando.DragOver*/>
+              subdestaque-13
+          </div>
+    </div>
+    )
+}
+/*#Export*/
+export { GridTFB }
+
