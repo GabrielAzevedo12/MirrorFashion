f629b45962508b6406505b3911e9feed9868733d
GabrielAzevedo12 Vieira <gazevedo996@gmail.com>
2023-07-03 18:57:12 +0000

    Mon Jul  3 06:57:12 PM UTC 2023

--- src/Slide/componentsSlide/gridTD/1/1.js
+++ src/Slide/componentsSlide/gridTD/1/1.js
@@ -1,5 +1,8 @@
 /*#Comentários*/
 // Este é o Hook GridTD1
+//<GridTF6/>
+//<GridTF7/>
+//<GridTF8/>
 /*Comentários#*/
 
 /*#Imports*/
@@ -12,11 +15,18 @@ import {
   Diminuir_ElementoPai,
   gridTN,
 } from '../../DBanner/f/f.js';
-import { GridTD1F1 } from './gtd1f1/gtd1fh1';
-import { GridTD1F2 } from './gtd1f2/gtd1fh2';
-import { GridTD1F3 } from './gtd1f3/gtd1fh3';
-import { GridTD1FB } from './gtd1fB/gtd1fhB.js'
-import { HerençaOf_GridTD1 } from './hgtd1/hgtd1.js';
+import { GridTF1 } from './gtf1/gtfh1';
+import { GridTF2 } from './gtf2/gtfh2';
+import { GridTF3 } from './gtf3/gtfh3';
+import { GridTF4 } from './gtf4/gtfh4';
+import { GridTF5 } from './gtf5/gtfh5';
+import { GridTF6 } from './gtf6/gtfh6';
+import { GridTF7 } from './gtf7/gtfh7';
+import { GridTF8 } from './gtf8/gtfh8';
+import { GridTF9 } from './gtf9/gtfh9';
+import { GridTF10 } from './gtf10/gtfh10';
+import { GridTF11 } from './gtf11/gtfh11';
+import { GridTFB } from './gtfB/gtfhB.js'
 
 /*Imports#*/
 
@@ -29,18 +39,22 @@ const GridTD1 = () => {
   /*#EscopoGridTD1*/
 
   const 
-  display_none = { display: 'none' },
-  Herença = {HerençaOf_GridTD1} ;
+  display_none = { display: 'none' } ;
 
   /*EscopoGridTD1#*/
 
   /*#HookCont1*/
   return (
-    <div className='gridTD1'>
-      <GridTD1FB Obj_Pai_Props={Herença}/>
-      <GridTD1F1 Obj_Pai_Props={Herença}/>
-      <GridTD1F2 Obj_Pai_Props={Herença}/>
-      <GridTD1F3 Obj_Pai_Props={Herença}/>
+    <div className='gridTD'>
+      <GridTF1/>
+      <GridTF2/>
+      <GridTF3/>
+      <GridTF4/>
+      <GridTF5/>
+      <GridTF9/>
+      <GridTF10/>
+      <GridTF11/>
+      <GridTFB/>
     </div>
   );
   /*HookCont1#*/
