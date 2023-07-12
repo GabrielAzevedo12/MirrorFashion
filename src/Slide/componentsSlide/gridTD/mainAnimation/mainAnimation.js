254c27dba8a6db6cb027af40384fdad64986995e
gazevedo996 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-07-12 18:29:04 +0000

    Wed 12 Jul 2023 06:29:04 PM UTC

--- src/Slide/componentsSlide/gridTD/mainAnimation/mainAnimation.js
+++ src/Slide/componentsSlide/gridTD/mainAnimation/mainAnimation.js
@@ -34,25 +34,16 @@ var DivAnimationMain = () => {
       <h1 className="Vindo"> Vindo </h1>
       <h1 className="aMirrorFashion"> a MirrorFashion </h1>
 
-      <div className="main_banner_1 main_banner_1_state_inical">
-        {" "}
-        main banner 1{" "}
+      <div className="main_banner_1 displayNone main_banner_1_state_inical">
+        main banner 1
       </div>
-      <div className="main_banner_2 main_banner_2_state_inical">
-        {" "}
-        main banner 2{" "}
+      <div className="main_banner_2 displayNone">main banner 2</div>
+      <div className="main_banner_3 displayNone">main banner 3</div>
+      <div className="main_banner_4 displayNone main_banner_4_state_inical">
+        main banner 4
       </div>
-      <div className="main_banner_3 main_banner_3_state_inical">
-        {" "}
-        main banner 3{" "}
-      </div>
-      <div className="main_banner_4 main_banner_4_state_inical">
-        {" "}
-        main banner 4{" "}
-      </div>
-      <div className="main_banner_5 main_banner_5_state_inical">
-        {" "}
-        main banner 5{" "}
+      <div className="main_banner_5 displayNone main_banner_5_state_inical">
+        main banner 5
       </div>
     </div>
   );
